8c62dc850bd59258f726e1e18916ed70e4090500
ynccsyd <muhibbi_10_@hotmaile.com>
2022-12-08 13:32:23 +0300

    Added project

--- README.md
+++ README.md
@@ -1,48 +1,49 @@
-# Give your token an identity
+# Solana-Token-Metadata
 ## $\textcolor{green}{Getting\ Started}$
 
- ### :dart: $\textcolor{gray}{In\ this\ project\ you\ have\ to\ know:}$ 
-<div>
-     <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg" title="typescript" **alt="typescript" width="40" height="40"/> 
-</div>
-
----
-
-### :memo: $\textcolor{gray}{ First,\ run\ the\ development\ server:}$ 
- 
+First, run the development server:
 ```bash
+
+npm install @solana/web3.js
 npm install @metaplex-foundation/js fs
 npm install @metaplex-foundation/mpl-token-metadata
 npm start
 ```
----
-
-### :speech_balloon:  $\textcolor{gray}{ Be\ creative!!!\ Write\ your\ token\ details:}$ 
-
-````ruby
-const tokenName = "MangırCoin"
-const description = "Welcome to rebirth of the everything."
+ ---
+ ### $\textcolor{green}{Let's\ start}$
+  - First describe your token metada:
+  ```ruby
+const tokenName = "Paracoin"
+const description = "Welcome to the rebirth of everything."
 const symbol = "PRC"
 const decimals = 2
-const amount = 1000
-````
----
-### :grey_exclamation:  $\textcolor{gray}{ Do\ not\ forget\ to\ add\ your\ token\ image}$ 
+const amount = 1
 
-````ruby
-  // file to buffer
-  const buffer = fs.readFileSync("src/images.png")
+```
+#### $\textcolor{red}{Do\ not\ forget\ to\ add\ your\ image\ in\ SRC!}$ :v:
+ ```ruby
+// file to buffer
+  const buffer = fs.readFileSync("src/phoenix.jpg")
 
   // buffer to metaplex file
-  const file = toMetaplexFile(buffer, "images.png")
-  ````
- ## :trophy: $\textcolor{brown}{Congratulations\, now\ you\ have\ your\ token!}$
+  const file = toMetaplexFile(buffer, "phoenix.jpg")
+
+  // upload image and get image uri
+  const imageUri = await metaplex.storage().upload(file)
+  console.log("image uri:", imageUri)
+
+```
+### :tada:  $\textcolor{green}{ And\ run\ code\, here\ is\ your\ METADATA :}$ 
+ 
+````json
+{
+"name":"Paracoin",
+"description":"Welcome to the rebirth of everything.",
+"image":"https://arweave.net/rdkW0-WL8RVPOxvkDk7VnHv2Z9aofQuB16VmEhT7LnI"
+}
+````
+
 <p align="center">
-  <img src="https://user-images.githubusercontent.com/109158340/206389892-fe3020bf-574b-4b5a-bf6a-c2fa2a5ce8b3.png" >
- 
- ###   $\textcolor{gray}{ Here's\ your\ token's\ metadata}$ 
- 
- <p align="center">
-  <img src="https://user-images.githubusercontent.com/109158340/206390331-6046ac77-171d-49c7-82a8-b8c308101190.png" >
- 
+    <img src="https://arweave.net/rdkW0-WL8RVPOxvkDk7VnHv2Z9aofQuB16VmEhT7LnI" width="200" height="200">
+
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import { initializeKeypair } from "./initializeKeypair"
-import * as web3 from "@solana/web3.js"
-// import * as token from "@solana/spl-token"
+import {
+  Connection,
+  clusterApiUrl,
+  Transaction,
+  sendAndConfirmTransaction,
+  Keypair,
+  SystemProgram,
+} from "@solana/web3.js"
+import {
+  createInitializeMintInstruction,
+  getMinimumBalanceForRentExemptMint,
+  getAssociatedTokenAddress,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+  createAssociatedTokenAccountInstruction,
+  Account,
+  TokenAccountNotFoundError,
+  TokenInvalidAccountOwnerError,
+  getAccount,
+  createMintToInstruction,
+} from "@solana/spl-token"
 import {
   Metaplex,
   keypairIdentity,
@@ -11,25 +30,52 @@ import {
 import {
   DataV2,
   createCreateMetadataAccountV2Instruction,
-  createUpdateMetadataAccountV2Instruction,
 } from "@metaplex-foundation/mpl-token-metadata"
 import * as fs from "fs"
 
+const tokenName = "MangırCoin"
+const description = "Welcome to rebirth of the everything."
+const symbol = "PRC"
+const decimals = 2
+const amount = 1000
 
-async function createTokenMetadata(
-  connection: web3.Connection,
-  metaplex: Metaplex,
-  mint: web3.PublicKey,
-  user: web3.Keypair,
-  name: string,
-  symbol: string,
-  description: string
-) {
+async function main() {
+  const connection = new Connection(clusterApiUrl("devnet"))
+  const user = await initializeKeypair(connection)
+
+  console.log("PublicKey:", user.publicKey.toBase58())
+
+  // rent for token mint
+  const lamports = await getMinimumBalanceForRentExemptMint(connection)
+
+  // keypair for new token mint
+  const mintKeypair = Keypair.generate()
+
+  // get metadata PDA for token mint
+  const metadataPDA = await findMetadataPda(mintKeypair.publicKey)
+
+  // get associated token account address for use
+  const tokenATA = await getAssociatedTokenAddress(
+    mintKeypair.publicKey,
+    user.publicKey
+  )
+
+  // metaplex setup
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(user))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    )
+
   // file to buffer
-  const buffer = fs.readFileSync("assets/phoenix.jpg")
+  const buffer = fs.readFileSync("src/images.png")
 
   // buffer to metaplex file
-  const file = toMetaplexFile(buffer, "phoenix.jpg")
+  const file = toMetaplexFile(buffer, "images.png")
 
   // upload image and get image uri
   const imageUri = await metaplex.storage().upload(file)
@@ -39,19 +85,17 @@ async function createTokenMetadata(
   const { uri } = await metaplex
     .nfts()
     .uploadMetadata({
-      name: name,
+      name: tokenName,
       description: description,
       image: imageUri,
     })
- 
-  console.log("metadata uri:", uri)
+    .run()
 
-  // get metadata account address
-  const metadataPDA = await findMetadataPda(mint)
+  console.log("metadata uri:", uri)
 
   // onchain metadata format
   const tokenMetadata = {
-    name: name,
+    name: tokenName,
     symbol: symbol,
     uri: uri,
     sellerFeeBasisPoints: 0,
@@ -61,11 +105,28 @@ async function createTokenMetadata(
   } as DataV2
 
   // transaction to create metadata account
-  const transaction = new web3.Transaction().add(
+  const transaction = new Transaction().add(
+    // create new account
+    SystemProgram.createAccount({
+      fromPubkey: user.publicKey,
+      newAccountPubkey: mintKeypair.publicKey,
+      space: MINT_SIZE,
+      lamports: lamports,
+      programId: TOKEN_PROGRAM_ID,
+    }),
+    // create new token mint
+    createInitializeMintInstruction(
+      mintKeypair.publicKey,
+      decimals,
+      user.publicKey,
+      user.publicKey,
+      TOKEN_PROGRAM_ID
+    ),
+    // create metadata account
     createCreateMetadataAccountV2Instruction(
       {
         metadata: metadataPDA,
-        mint: mint,
+        mint: mintKeypair.publicKey,
         mintAuthority: user.publicKey,
         payer: user.publicKey,
         updateAuthority: user.publicKey,
@@ -79,48 +140,57 @@ async function createTokenMetadata(
     )
   )
 
+  // instruction to create ATA
+  const createTokenAccountInstruction = createAssociatedTokenAccountInstruction(
+    user.publicKey, // payer
+    tokenATA, // token address
+    user.publicKey, // token owner
+    mintKeypair.publicKey // token mint
+  )
+
+  let tokenAccount: Account
+  try {
+    // check if token account already exists
+    tokenAccount = await getAccount(
+      connection, // connection
+      tokenATA // token address
+    )
+  } catch (error: unknown) {
+    if (
+      error instanceof TokenAccountNotFoundError ||
+      error instanceof TokenInvalidAccountOwnerError
+    ) {
+      try {
+        // add instruction to create token account if one does not exist
+        transaction.add(createTokenAccountInstruction)
+      } catch (error: unknown) {}
+    } else {
+      throw error
+    }
+  }
+
+  transaction.add(
+    // mint tokens to token account
+    createMintToInstruction(
+      mintKeypair.publicKey,
+      tokenATA,
+      user.publicKey,
+      amount * Math.pow(10, decimals)
+    )
+  )
+
   // send transaction
-  const transactionSignature = await web3.sendAndConfirmTransaction(
+  const transactionSignature = await sendAndConfirmTransaction(
     connection,
     transaction,
-    [user]
+    [user, mintKeypair]
   )
 
   console.log(
-    `Create Metadata Account: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
   )
 }
-async function main() {
-  const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-  const user = await initializeKeypair(connection)
 
-  console.log("PublicKey:", user.publicKey.toBase58())
-
-  // MAKE SURE YOU REPLACE THIS ADDRESS WITH YOURS!
-  const MINT_ADDRESS = "FecuKGyRe3BEGbZkyYYbcTzTookSqBi2sEcd8hcWJ16Z"
-
-  // metaplex setup
-  const metaplex = Metaplex.make(connection)
-    .use(keypairIdentity(user))
-    .use(
-      bundlrStorage({
-        address: "https://devnet.bundlr.network",
-        providerUrl: "https://api.devnet.solana.com",
-        timeout: 60000,
-      })
-    )
-  
-  // Calling the token 
-  await createTokenMetadata(
-    connection,
-    metaplex,
-    new web3.PublicKey(MINT_ADDRESS),
-    user,
-    "ParaCoin", // Token name - REPLACE THIS WITH YOURS
-    "PRC",     // Token symbol - REPLACE THIS WITH YOURS
-    "welcome to the rebirth of everything." // Token description - REPLACE THIS WITH YOURS
-  )
-}
 main()
   .then(() => {
     console.log("Finished successfully")
